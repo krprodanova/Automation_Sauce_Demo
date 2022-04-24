3864a2a5f0bacfaf85f9c8ebb019176c66eb99a1
krprodanova <kremena.prodanov@abv.bg>
2022-04-24 14:09:16 +0300

    Initial commit

--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -10,8 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 public class CheckoutPage{
 public WebDriver driver;
 
-//private static final String CART_LINK = "//button[@id = 'shopping_cart_container']";
-//private static final String CHECKOUT_LOCATOR = "//button[@id = 'checkout']";
+
 
 
 @FindBy(className = "shopping_cart_link")
@@ -24,9 +23,7 @@ public CheckoutPage(WebDriver driver){
     this.driver = driver;
     PageFactory.initElements(driver, this);
 }
-//public void shoppingCartLink(){
-    //driver.findElement(By.className("shopping_cart_link")).click();
-    //}
+
     public void btnBtnActionBtnMediumCheckoutButton(){
     driver.findElement(By.className("btn btn_action btn_medium checkout_button")).click();
 
